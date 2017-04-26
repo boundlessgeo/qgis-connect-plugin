cae3f0705eae6f8478f873f52084250c7a0a0816
Alexander Bruy <alexander.bruy@gmail.com>
2017-04-26 19:27:43 +0300

    uninstall only newly installed plugins

--- boundlessconnect/tests/testerplugin.py
+++ boundlessconnect/tests/testerplugin.py
@@ -285,7 +285,7 @@ class BoundlessConnectTests(unittest.TestCase):
         for key in plugins.all():
             if key in ['boundlessconnect']:
                 continue
-            if utils.isBoundlessPlugin(plugins.all()[key]) and key not in installedPlugins:
+            if utils.isBoundlessPlugin(plugins.all()[key]) and plugins.all()[key]['installed'] and key not in installedPlugins:
                 installer.uninstallPlugin(key, quiet=True)
 
 
