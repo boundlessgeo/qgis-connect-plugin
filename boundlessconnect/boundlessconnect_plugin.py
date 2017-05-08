b119a5ee4fcd8340c4261fd0e770ff31456f6d44
Alexander Bruy <alexander.bruy@gmail.com>
2017-05-08 13:45:56 +0300

    use commons library in plugin drop unneeded i18n support

--- boundlessconnect/boundlessconnect_plugin.py
+++ boundlessconnect/boundlessconnect_plugin.py
@@ -39,6 +39,8 @@ from qgis.gui import QgsMessageBar, QgsMessageBarItem
 
 from pyplugin_installer.installer_data import (repositories,
                                                plugins)
+from qgiscommons.gui import askForFiles
+
 from boundlessconnect.gui.connectdockwidget import getConnectDockWidget
 from boundlessconnect import utils
 
@@ -48,7 +50,6 @@ pluginPath = os.path.dirname(__file__)
 class BoundlessConnectPlugin(object):
     def __init__(self, iface):
         self.iface = iface
-        self.dockWidget = None
 
         try:
             from boundlessconnect.tests import testerplugin
@@ -57,18 +58,7 @@ class BoundlessConnectPlugin(object):
         except Exception as e:
             pass
 
-        overrideLocale = QSettings().value('locale/overrideFlag', False, bool)
-        if not overrideLocale:
-            locale = QLocale.system().name()[:2]
-        else:
-            locale = QSettings().value('locale/userLocale', '')
-
-        qmPath = '{}/i18n/boundlessconnect_{}.qm'.format(pluginPath, locale)
-
-        if os.path.exists(qmPath):
-            self.translator = QTranslator()
-            self.translator.load(qmPath)
-            QCoreApplication.installTranslator(self.translator)
+        self.dockWidget = None
 
         self.iface.initializationCompleted.connect(self.checkFirstRun)
 
@@ -80,19 +70,18 @@ class BoundlessConnectPlugin(object):
         utils.setRepositoryUrl()
 
         self.actionRunWizard = self.dockWidget.toggleViewAction()
-        self.actionRunWizard.setText(self.tr('Boundless Connect'))
+        self.actionRunWizard.setText('Boundless Connect')
         self.actionRunWizard.setIcon(
             QIcon(os.path.join(pluginPath, 'icons', 'connect.svg')))
-        self.actionRunWizard.setWhatsThis(
-            self.tr('Boundless Connect'))
+        self.actionRunWizard.setWhatsThis('Boundless Connect')
         self.actionRunWizard.setObjectName('actionRunWizard')
 
         self.actionPluginFromZip = QAction(
-            self.tr('Install plugin from ZIP'), self.iface.mainWindow())
+            'Install plugin from ZIP', self.iface.mainWindow())
         self.actionPluginFromZip.setIcon(
             QIcon(os.path.join(pluginPath, 'icons', 'plugin.svg')))
         self.actionPluginFromZip.setWhatsThis(
-            self.tr('Install plugin from ZIP file stored on disk'))
+            'Install plugin from ZIP file stored on disk')
         self.actionPluginFromZip.setObjectName('actionPluginFromZip')
 
         self.actionPluginFromZip.triggered.connect(self.installPlugin)
@@ -101,15 +90,15 @@ class BoundlessConnectPlugin(object):
         # to start modified Plugin Manager which works with local repositories
         if utils.isRepositoryInDirectory():
             self.actionPluginManager = QAction(
-                self.tr('Manage plugins (local folder)'), self.iface.mainWindow())
+                'Manage plugins (local folder)', self.iface.mainWindow())
             self.actionPluginManager.setIcon(
                 QIcon(os.path.join(pluginPath, 'icons', 'plugin.svg')))
             self.actionPluginManager.setWhatsThis(
-                self.tr('Manage and install plugins from local repository'))
+                'Manage and install plugins from local repository')
             self.actionPluginManager.setObjectName('actionPluginManager')
 
             self.iface.addPluginToMenu(
-                self.tr('Boundless Connect'), self.actionPluginManager)
+                'Boundless Connect', self.actionPluginManager)
 
             self.actionPluginManager.triggered.connect(self.pluginManagerLocal)
 
@@ -171,26 +160,16 @@ class BoundlessConnectPlugin(object):
         self.dockWidget.askForAuth = True
 
     def installPlugin(self):
-        settings = QSettings('Boundless', 'BoundlessConnect')
-        lastDirectory = settings.value('lastPluginDirectory', '.')
+        fileName = askForFiles(self.iface.mainWindow(),
+                               'Open file',
+                               exts='zip')
 
-        if QT_VERSION_STR.startswith('5'):
-            fileName, _ = QFileDialog.getOpenFileName(self.iface.mainWindow(),
-                                                   self.tr('Open file'),
-                                                   lastDirectory,
-                                                   self.tr('Plugin packages (*.zip *.ZIP)'))
-        else:
-            fileName = QFileDialog.getOpenFileName(self.iface.mainWindow(),
-                                                   self.tr('Open file'),
-                                                   lastDirectory,
-                                                   self.tr('Plugin packages (*.zip *.ZIP)'))
-
-        if fileName == '':
+        if fileName is None:
             return
 
         result = utils.installFromZipFile(fileName)
         if result is None:
-            self._showMessage(self.tr('Plugin installed successfully'),
+            self._showMessage('Plugin installed successfully',
                               QgsMessageBar.SUCCESS)
         else:
             self._showMessage(result, QgsMessageBar.WARNING)
@@ -204,6 +183,3 @@ class BoundlessConnectPlugin(object):
     def _showMessage(self, message, level=QgsMessageBar.INFO):
         self.iface.messageBar().pushMessage(
             message, level, self.iface.messageTimeout())
-
-    def tr(self, text):
-        return QCoreApplication.translate('Boundless Connect', text)
