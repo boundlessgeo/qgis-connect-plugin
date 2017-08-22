7453cb04efde0c361cac9b6554d75fd443c1d276
Luigi Pirelli <luipir@gmail.com>
2017-08-22 12:19:10 +0200

    ext-libs in extlibs

--- pavement.py
+++ pavement.py
@@ -18,7 +18,7 @@ options(
         name = 'boundlessconnect',
         source_dir = path('boundlessconnect'),
         package_dir = path('.'),
-        ext_libs = path('boundlessconnect/ext-libs'),
+        ext_libs = path('boundlessconnect/extlibs'),
         ext_src = path('boundlessconnect/ext-src'),
         tests = ['test'],
         excludes = [
@@ -200,7 +200,7 @@ def pep8(args):
     ignore = ['E203', 'E121', 'E122', 'E123', 'E124', 'E125', 'E126', 'E127',
         'E128', 'E402']
     styleguide = pep8.StyleGuide(ignore=ignore,
-                                 exclude=['*/ext-libs/*', '*/ext-src/*'],
+                                 exclude=['*/extlibs/*', '*/ext-src/*'],
                                  repeat=True, max_line_length=79,
                                  parse_argv=args)
     styleguide.input_dir(options.plugin.source_dir)
